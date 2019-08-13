91fd214cbfb94320a80191ffbe6dde375ce4be79
Jeevan:M <csejeevan@gmail.com>
2019-08-13 18:16:17 +0530

    problem statment

--- complex/entery.py
+++ complex/entery.py
@@ -13,9 +13,11 @@ for i in range(num_of_entery):
 
 print(outer_lst)
 """
-array_of_input =[[1526579928,3,'enter'],[1526579982,2,'exit'],[1526579991,6,'enter'],[1526579994,1,'exit'],[1526579999,2,'enter']]
-dummy = array_of_input
+
+array_of_input = [[1526579928,3,'enter'],[1526579982,2,'exit'],[1526579991,6,'enter'],[1526579994,1,'exit'],[1526579999,2,'enter']]
+
+
 for i in range(len(array_of_input),-1):
     for j in range(1,len(array_of_input)):
         if array_of_input[i][2] == 'enter':
-            
+                

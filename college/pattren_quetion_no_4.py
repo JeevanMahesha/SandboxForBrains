814bd872bf93f24323bb09a65a05c3ab00d0f9db
Jeevan-M <34814862+Jeevan-M@users.noreply.github.com>
2019-08-19 09:58:37 +0530

    hackerrank  problem solved

--- college/pattren_quetion_no_4.py
+++ college/pattren_quetion_no_4.py
@@ -1,9 +1,18 @@
-for i in range(num,0,-1):
-    for j in range(i):
-        if flag%2 == 0:
-            print(' ',num1,end="")
-            num1 +=1
-        else:
-            print(' ',chr(strr),end="")
-            strr +=1         
-    print('\n')
+
+def Diamond(rows):
+    for i in range(rows):
+        for j in range(rows-i-1):
+            print(end=" ")
+        for j in range(i+1):
+            print('*',end=" ")
+        print('\n')
+    for i in range(rows-2,0,-1):
+        for j in range(rows-i-1):
+            print(end=" ")
+        for j in range(i+1):
+            print('*',end=" ")
+        print('\n')
+      
+        
+rows = 5
+Diamond(rows)

e8a97fcd21de84450b5c0842d9bb950d5113b1e2
Jeevan-M <csejeevanm@gmail.com>
2020-01-17 17:49:56 +0530

    patterns

--- pratice_pattern/odd_numbers_of_star.py
+++ pratice_pattern/odd_numbers_of_star.py
@@ -1,9 +1,9 @@
-n = 5
+n = 6
 c = 1
 for row in range(1, n + 1):
-    for col_spc in range(n - row):
-        print(end=" ")
+    for col_space in range(n - row):
+        print(end="    ")
     for col in range(1, c + 1):
-        print(c, end="")
+        print('*', end=" ")
     c += 2
     print()

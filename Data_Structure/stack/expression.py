1d91b18c90f08ed4d9eca2f052733ffbf0e9463b
Jeevan-M <csejeevanm@gmail.com>
2020-03-10 09:45:42 +0530

    updated

---
+++ Data_Structure/stack/expression.py
@@ -0,0 +1,13 @@
+def exp(value):
+    print()
+    temp = list()
+    for i in value:
+        if i.isdigit():
+            temp.append(i)
+    
+
+
+value = list('10 + 2 * 6')
+print(value)
+
+exp(value)

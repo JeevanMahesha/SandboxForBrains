111f65f682a0111212cbf3803ee92fe28d28a4b1
Jeevan-M <csejeevanm@gmail.com>
2020-03-14 08:51:16 +0530

    waterworld

---
+++ college/ajira/WaterWorld_using_oops.py
@@ -0,0 +1,11 @@
+class Tank:
+    def __init__(self,clusters,day_use,capacity):
+        self.Clusters = clusters
+        self.Day_Use  = day_use 
+        self.Capacity = capacity
+
+    
+if __name__ == "__main__":
+    Number_of_days = int(input())
+    TankObject = list()
+    
--- college/ajira/demo.py
+++ college/ajira/demo.py
@@ -6,7 +6,6 @@ def requiredWaterForGivenDays(Number_Of_Days,clusters,connection):
     print(capacity_of_each_cluster)
     ans = [v for k,v in capacity_of_each_cluster.items()]
     for i in range(Number_Of_Days):
-        temp = []
         print('Day :',i+1)
         for j in connection:
             for k in range(len(j)-1,-1,-1):
@@ -23,32 +22,15 @@ def requiredWaterForGivenDays(Number_Of_Days,clusters,connection):
                                if j[l] in capacity_of_each_cluster.keys():
                                    value = capacity_of_each_cluster.get(j[l])
                                    ans.append(value)
-                                   temp.append(value)
                                    clusters.get(j[k]).update({key:value})
                                    clusters.get(j[k]).update({key:value-key})
                            print(j[k],'=',clusters.get(j[k]),'after update')
-        print(temp)
         print()
     print(sum(ans))
+  
 if __name__ == "__main__":    
-    Number_Of_Days = int(input())
-    clusters = {}
-    for i in range(int(input())):
-        Temp_dict = {}
-        input_value = input().split()
-        Temp_dict[int(input_value[1])] = int(input_value[2])
-        clusters [input_value[0]] = Temp_dict
-    connection = list()
-    for i in range(int(input())):
-        input_value = input().split('_')
-        if input_value[0] == 'F':
-            connection.append(input_value)
-        else:
-            for i in connection:
-                if input_value[0] in i:
-                    i.append(input_value[1])
-    print(Number_Of_Days)
-    print(clusters)
-    print(connection)
+    Number_Of_Days = 5
+    clusters = {'c1': {200: 300}, 'c2': {150: 300}, 'c3': {200: 200}, 'c4': {100: 400}}
+    connection = [['F', 'c1', 'c2'], ['F', 'c3', 'c4']]
     requiredWaterForGivenDays(Number_Of_Days,clusters,connection)
     

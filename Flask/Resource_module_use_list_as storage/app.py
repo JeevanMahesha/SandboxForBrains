c8619fe4c71c93f80d177f563f040eea470928f5
Jeevan-M <csejeevanm@gmail.com>
2020-04-21 12:34:47 +0530

    starting the project with Database

--- Flask/Resource_module_use_list_as storage/app.py
+++ Flask/Resource_module_use_list_as storage/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-from flask_restful import Resource,Api
+from flask_restful import Resource,Api,reqparse
 from flask_jwt import jwt_required,JWT
 from security import authenticate,identity
 
@@ -12,6 +12,13 @@ items_list = []
 jwt = JWT(app,authenticate,identity) #/auth
 
 class Items(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'Price',
+        type=float,
+        required=True,
+        help='This Field is required'
+    )
     @jwt_required()
     def get(self,name):
         items = next(filter(lambda item_name: item_name['name'] == name,items_list),None)
@@ -20,7 +27,8 @@ class Items(Resource):
     def post(self,name):
         if next(filter(lambda item_name: item_name['name'] == name,items_list),None):
             return {'Message':f'The item with name {name} is already exisit'},400
-        request_data = request.get_json()
+        
+        request_data = Items.parser.parse_args()
         items_list.append({'name':name,'Price':request_data['Price']})
         return items_list,201
 
@@ -30,7 +38,7 @@ class Items(Resource):
         return {'Message':'Items Deleted'}
 
     def put(self,name):
-        request_data = request.get_json()
+        request_data = Items.parser.parse_args()
         item = next(filter(lambda it: it['name'] == name,items_list),None)
         if item is None:
             item = {'name':name,'Price':request_data['Price']}
@@ -46,6 +54,5 @@ class ItemsList(Resource):
 
 API.add_resource(Items,'/item/<string:name>')
 API.add_resource(ItemsList,'/items')
-
 
 app.run(debug=True)

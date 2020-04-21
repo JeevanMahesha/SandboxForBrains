1f0cf2a118eb23c333225ef1aa9fad112bfdc322
Jeevan-M <csejeevanm@gmail.com>
2020-04-21 11:44:31 +0530

    tocken and delete finished

--- Flask/Resource_module/app.py
+++ Flask/Resource_module/app.py
@@ -1,11 +1,18 @@
 from flask import Flask,request
 from flask_restful import Resource,Api
+from flask_jwt import jwt_required,JWT
+from security import authenticate,identity
+
 
 app = Flask(__name__)
 API = Api(app)
+app.secret_key = 'jeevan'
 items_list = []
 
+jwt = JWT(app,authenticate,identity) #/auth
+
 class Items(Resource):
+    @jwt_required()
     def get(self,name):
         items = next(filter(lambda item_name: item_name['name'] == name,items_list),None)
         return{'items':items} ,200 if items else  404
@@ -16,6 +23,11 @@ class Items(Resource):
         request_data = request.get_json()
         items_list.append({'name':name,'Price':request_data['Price']})
         return items_list,201
+
+    def delete(self,name):
+        global items_list
+        items_list = list(filter(lambda  x: x['name'] != name,items_list))
+        return {'Message':'Items Deleted'}
 
 class ItemsList(Resource):
     def get(self):

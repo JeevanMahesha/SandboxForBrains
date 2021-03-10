afe0b4ff5835b8f78c1bee1afe928942b624e9c1
Jeevan-M <csejeevanm@gmail.com>
2021-03-10 11:18:33 +0530

    Login UI metrial

---
+++ src/app/login/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  roleArray: Array<string> = ['Developer', 'HR', 'Tester', 'Manager']
+  userEmail: string
+  userPassword: string
+  userRole: string
+  userStatus: object
+  @Input() userDetailsDB: any
+  @Output() userStatusEmit: any = new EventEmitter()
+  constructor() { }
+
+  ngOnInit(): void {
+    // console.log(this.userDetailsDB);
+  }
+
+  getEmail(email: string) {
+    this.userEmail = email
+
+  }
+  getPassword(password: string) {
+    this.userPassword = password
+  }
+
+  selectRole(role) {
+    this.userRole = role
+
+  }
+
+  sendUserDetails($event) {
+    if (this.userDetailsDB.userEmail == this.userEmail && this.userDetailsDB.userPassword == this.userPassword) {
+      this.userStatus = { 'status': true, 'data': 'Sucess' }
+    } else {
+      this.userStatus = { 'status': false, 'data': 'Failed' }
+    }
+    this.userStatusEmit.emit(this.userStatus)
+  }
+
+
+}

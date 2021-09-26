3cfe454d7c2936cff27e2ef5f5af1c62d24604d8
Jeevan M <csejeevanm@gmail.com>
2021-09-26 12:22:42 +0530

    unsubscribe the observables

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
+  pathSubscription: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
@@ -16,13 +18,19 @@ export class UserComponent implements OnInit {
       id: this.activatedRoute.snapshot.params["id"],
       name: this.activatedRoute.snapshot.params["name"],
     };
-    this.activatedRoute.params.subscribe((pathParams: Params) => {
-      this.user.id = pathParams["id"];
-      this.user.name = pathParams["name"];
-    });
+    this.pathSubscription = this.activatedRoute.params.subscribe(
+      (pathParams: Params) => {
+        this.user.id = pathParams["id"];
+        this.user.name = pathParams["name"];
+      }
+    );
   }
 
   loadTheUser() {
     this.router.navigate(["/users", 10, "latha"]);
+  }
+
+  ngOnDestroy(): void {
+    this.pathSubscription.unsubscribe();
   }
 }

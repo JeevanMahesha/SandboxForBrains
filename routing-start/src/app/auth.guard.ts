026ed7222ca98d8786d18fcea1ad0623cc6d548e
Jeevan M <csejeevanm@gmail.com>
2021-09-26 20:15:27 +0530

    canActivateChild implementation

--- routing-start/src/app/auth.guard.ts
+++ routing-start/src/app/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   Router,
   RouterStateSnapshot,
   UrlTree,
@@ -12,10 +13,10 @@ import { AuthService } from "./auth.service";
 @Injectable({
   providedIn: "root",
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
   canActivate(
-    route: ActivatedRouteSnapshot,
+    routes: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ):
     | Observable<boolean | UrlTree>
@@ -26,9 +27,20 @@ export class AuthGuard implements CanActivate {
       .isAuthenticated()
       .then((authentication: boolean) => {
         if (!authentication) {
+          alert("please login");
           this.router.navigate(["/"]);
         }
         return true;
       });
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
   }
 }

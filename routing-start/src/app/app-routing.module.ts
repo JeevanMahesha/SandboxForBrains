3e3903d6a278ea3712aaaa67a8b8e843d549d3a4
Jeevan M <csejeevanm@gmail.com>
2021-09-26 21:52:35 +0530

    Understanding Location Strategies

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -55,7 +55,7 @@ const appRouting: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRouting)],
+  imports: [RouterModule.forRoot(appRouting, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}

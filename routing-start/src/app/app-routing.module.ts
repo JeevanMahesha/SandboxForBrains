c8b0ea93a717ad1c577b288e9a1a9488132cf94a
Jeevan M <csejeevanm@gmail.com>
2021-09-26 11:55:40 +0530

    Passing Parameters to Routes

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
 import { UsersComponent } from "./users/users.component";
 
 const appRouting: Routes = [
   { path: "", component: HomeComponent },
   { path: "users", component: UsersComponent },
+  { path: "users/:id", component: UserComponent },
   { path: "servers", component: ServersComponent },
 ];
 

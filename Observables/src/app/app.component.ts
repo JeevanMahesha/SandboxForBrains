92da4b81a8d55c93caf7f27db789a43b5b0a24b5
Jeevan M <csejeevanm@gmail.com>
2021-10-03 18:12:38 +0530

    Subjects in angular

--- Observables/src/app/app.component.ts
+++ Observables/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { AppService } from "./app.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  isActivated: boolean = false;
+  constructor(private appService: AppService) {}
+  ngOnDestroy(): void {
+    this.appService.activatedEmitter.unsubscribe();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.appService.activatedEmitter.subscribe(
+      (activateStatus) => (this.isActivated = activateStatus)
+    );
+  }
 }

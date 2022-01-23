a1ea7fc0022b265a5b43b035fccdae7a4b2af362
Jeevan M <csejeevanm@gmail.com>
2022-01-23 20:40:19 +0530

    td assessment component created

---
+++ Template-Driven/src/app/tdassessment/tdassessment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tdassessment',
+  templateUrl: './tdassessment.component.html',
+  styleUrls: ['./tdassessment.component.css']
+})
+export class TdassessmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

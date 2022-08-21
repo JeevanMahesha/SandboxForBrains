7e65684286ebf4c19f98aad1c22158575336a8f6
Jeevan M <csejeevanm@gmail.com>
2022-08-21 14:43:23 +0530

    Creating a Custom Pipe

--- Pipes/src/app/app.module.ts
+++ Pipes/src/app/app.module.ts
@@ -1,19 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ShortenPipe } from "./CustomPipes/shorten.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, ShortenPipe],
+	imports: [BrowserModule, FormsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

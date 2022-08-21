1851505c5962b89e9c22c31fe8fc6c971c9ce371
Jeevan M <csejeevanm@gmail.com>
2022-08-21 18:32:14 +0530

    post request

--- http-requests/src/app/app.component.ts
+++ http-requests/src/app/app.component.ts
@@ -1,28 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+	loadedPosts = [];
+	fireBaseURL =
+		"https://angulardemoapi-4fa31-default-rtdb.asia-southeast1.firebasedatabase.app/";
+	jsonFileName = ".json";
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
-  }
+	onCreatePost(postData: { title: string; content: string }) {
+		// Send Http request
+		console.log(postData);
+		const url = this.getUrl("posts");
+		this.http.post(url, postData).subscribe((response) => {
+			console.log(response);
+		});
+	}
 
-  onFetchPosts() {
-    // Send Http request
-  }
+	onFetchPosts() {
+		// Send Http request
+	}
 
-  onClearPosts() {
-    // Send Http request
-  }
+	onClearPosts() {
+		// Send Http request
+	}
+
+	private getUrl(path: string): string {
+		return this.fireBaseURL + path + this.jsonFileName;
+	}
 }

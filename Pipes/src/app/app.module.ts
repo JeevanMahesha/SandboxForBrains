3265e3643bce281607d6ab312d0931bbd0e9bb03
Jeevan M <csejeevanm@gmail.com>
2022-08-21 15:31:57 +0530

    Assignment: Practicing Pipes

--- Pipes/src/app/app.module.ts
+++ Pipes/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ShortenPipe } from "./CustomPipes/shorten.pipe";
 import { FilterPipe } from './CustomPipes/filter.pipe';
+import { SortPipe } from './CustomPipes/sort.pipe';
+import { ReversePipe } from './CustomPipes/reverse.pipe';
 
 @NgModule({
-	declarations: [AppComponent, ShortenPipe, FilterPipe],
+	declarations: [AppComponent, ShortenPipe, FilterPipe, SortPipe, ReversePipe],
 	imports: [BrowserModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],

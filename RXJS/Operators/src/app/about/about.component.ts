1dc493926da086e445ab3da4afa5f20c2994a3ee
Jeevan M <csejeevanm@gmail.com>
2022-09-06 15:05:37 +0530

    Observable Concatenation

--- RXJS/Operators/src/app/about/about.component.ts
+++ RXJS/Operators/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { concat, of } from "rxjs";
 
 @Component({
 	selector: "about",
@@ -9,4 +10,15 @@ export class AboutComponent implements OnInit {
 	constructor() {}
 
 	ngOnInit() {}
+}
+
+function concatInRxjs() {
+	// the concat will waiting until the pervious observable to finish
+	const source1$ = of(1, 2, 3);
+
+	const source2$ = of(4, 5, 6);
+
+	const result$ = concat(source1$, source2$);
+
+	result$.subscribe(console.log);
 }

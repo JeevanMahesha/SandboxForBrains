b2bf2766a29acce0d307bc804f1ef0b1c2acff75
Jeevan M <csejeevanm@gmail.com>
2022-09-06 14:17:08 +0530

    imperative design

--- RXJS/Operators/src/app/about/about.component.ts
+++ RXJS/Operators/src/app/about/about.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { noop } from "rxjs";
-import { map } from "rxjs/operators";
-import { createHttpObservable } from "../common/util";
 
 @Component({
 	selector: "about",
@@ -11,14 +8,5 @@ import { createHttpObservable } from "../common/util";
 export class AboutComponent implements OnInit {
 	constructor() {}
 
-	ngOnInit() {
-		// RXJS stands for Reactive Extensions for JavaScript
-		const http$ = createHttpObservable("/api/courses");
-		const courses$ = http$.pipe(map((res) => res["payload"]));
-		courses$.subscribe(
-			(arg: any) => console.log(arg),
-			noop,
-			() => console.log("completed")
-		);
-	}
+	ngOnInit() {}
 }
--- RXJS/Operators/src/app/home/home.component.ts
+++ RXJS/Operators/src/app/home/home.component.ts
@@ -1,25 +1,36 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from "@angular/core";
+import { noop } from "rxjs";
+import { map } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
+import { Course } from "../model/course";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+	selector: "home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+	beginnersCourses: Course[] = [];
+	advancedCourses: Course[] = [];
 
+	constructor() {}
 
-    constructor() {
+	ngOnInit() {
+		// RXJS stands for Reactive Extensions for JavaScript
+		const http$ = createHttpObservable("/api/courses");
+		const courses$ = http$.pipe(map((res) => res["payload"]));
+		courses$.subscribe(
+			(courses: Course[]) => {
+				this.beginnersCourses = courses.filter(
+					(eachCourse) => eachCourse.category === "BEGINNER"
+				);
 
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+				this.advancedCourses = courses.filter(
+					(eachCourse) => eachCourse.category === "ADVANCED"
+				);
+			},
+			noop,
+			() => console.log("completed")
+		);
+	}
 }

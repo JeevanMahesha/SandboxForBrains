dd557826498be9a8fa6a127dbc4328aaee0cf2c9
Jeevan M <csejeevanm@gmail.com>
2022-09-06 14:55:44 +0530

    Sharing HTTP Responses with the shareReplay Operator

--- RXJS/Operators/src/app/home/home.component.ts
+++ RXJS/Operators/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { noop, Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay, tap } from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
 import { Course } from "../model/course";
 
@@ -17,9 +17,12 @@ export class HomeComponent implements OnInit {
 
 	ngOnInit() {
 		// RXJS stands for Reactive Extensions for JavaScript
+		// ! shareReplay will make a call once to take the data and give the subscribe variable when every / how many times they subscribe
 		const http$ = createHttpObservable("/api/courses");
 		const courses$: Observable<Course[]> = http$.pipe(
-			map((res) => res["payload"])
+			tap(() => console.info("HTTP Execution Done")),
+			map((res) => res["payload"]),
+			shareReplay()
 		);
 		this.beginnersCourses$ = courses$.pipe(
 			map((courses) =>

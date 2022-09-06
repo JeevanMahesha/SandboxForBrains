8a6a23da50d26e3a1cf4c18f164c6546d02c11ef
Jeevan M <csejeevanm@gmail.com>
2022-09-06 12:19:36 +0530

    basic custom Observable in RXJS

--- RXJS/Operators/src/app/about/about.component.ts
+++ RXJS/Operators/src/app/about/about.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { noop, Observable } from "rxjs";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+	selector: "about",
+	templateUrl: "./about.component.html",
+	styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {
+		// RXJS stands for reactive ex
+		const http$ = new Observable((observer) => {
+			fetch("/api/courses")
+				.then((res) => res.json())
+				.then((jsonValue) => {
+					observer.next(jsonValue);
+					observer.complete();
+				})
+				.catch((err) => observer.error(err));
+		});
 
-  ngOnInit() {
-  }
-
+		http$.subscribe(
+			(arg: any) => console.log(arg),
+			noop,
+			() => console.log("completed")
+		);
+	}
 }

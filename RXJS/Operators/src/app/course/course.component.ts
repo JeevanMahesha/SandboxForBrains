bb0ce10223ad542a6841feb450fec1c7089aa6ea
Jeevan M <csejeevanm@gmail.com>
2022-09-07 09:43:20 +0530

    custom RxJs Operator

--- RXJS/Operators/src/app/course/course.component.ts
+++ RXJS/Operators/src/app/course/course.component.ts
@@ -24,6 +24,7 @@ import {
 import { merge, fromEvent, Observable, concat, of } from "rxjs";
 import { Lesson } from "../model/lesson";
 import { createHttpObservable } from "../common/util";
+import { debug, RxJsLoggingLevel } from "../common/debug";
 
 @Component({
 	selector: "course",
@@ -41,16 +42,20 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
 	ngOnInit() {
 		this.courseId = this.route.snapshot.params["id"];
-		this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+		this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
+			debug(RxJsLoggingLevel.INFO, "course value")
+		);
 	}
 
 	ngAfterViewInit() {
 		this.lessons$ = fromEvent(this.input.nativeElement, "keyup").pipe(
 			map((event: KeyboardEvent) => event.target["value"]),
+			debug(RxJsLoggingLevel.TRACE, "search"),
 			startWith(""),
 			debounceTime(400),
 			distinctUntilChanged(),
-			switchMap(this.loadLessons.bind(this))
+			switchMap(this.loadLessons.bind(this)),
+			debug(RxJsLoggingLevel.DEBUG, "Lessons value")
 		);
 
 		// const initialLesson$ = this.loadLessons();

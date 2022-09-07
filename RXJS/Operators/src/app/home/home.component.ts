18885e6533f08315eb1916dd6745b113ba1c3a31
Jeevan M <csejeevanm@gmail.com>
2022-09-07 08:50:42 +0530

    retryWhen and delayWhen

--- RXJS/Operators/src/app/home/home.component.ts
+++ RXJS/Operators/src/app/home/home.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-import { noop, Observable, throwError } from "rxjs";
-import { catchError, finalize, map, shareReplay, tap } from "rxjs/operators";
+import { noop, Observable, throwError, timer } from "rxjs";
+import {
+	catchError,
+	delay,
+	delayWhen,
+	finalize,
+	map,
+	retryWhen,
+	shareReplay,
+	tap,
+} from "rxjs/operators";
 import { createHttpObservable } from "../common/util";
 import { Course } from "../model/course";
 
@@ -20,16 +29,10 @@ export class HomeComponent implements OnInit {
 		// ! shareReplay will make a call once to take the data and give the subscribe variable when every / how many times they subscribe
 		const http$ = createHttpObservable("/api/courses");
 		const courses$: Observable<Course[]> = http$.pipe(
-			catchError((err) => {
-				console.log(err);
-				return throwError(err);
-			}),
-			finalize(() => {
-				console.log("Finalize ...!");
-			}),
 			tap(() => console.info("HTTP Execution Done")),
 			map((res) => res["payload"]),
-			shareReplay()
+			shareReplay(),
+			retryWhen((error) => error.pipe(delayWhen(() => timer(2000))))
 		);
 		this.beginnersCourses$ = courses$.pipe(
 			map((courses) =>

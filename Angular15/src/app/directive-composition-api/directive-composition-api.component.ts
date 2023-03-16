739d27a3e334a1308d6ad06f99c5859ba56b92af
Jeevan <csejeevanm@gmail.com>
2023-03-16 15:45:29 +0530

    DirectiveCompositionAPIComponent -> template removed and added inside the ts file

--- Angular15/src/app/directive-composition-api/directive-composition-api.component.ts
+++ Angular15/src/app/directive-composition-api/directive-composition-api.component.ts
@@ -6,8 +6,67 @@ import { StyleDirective } from './style.directive';
 @Component({
   selector: 'app-directive-composition-api',
   standalone: true,
-  templateUrl: './directive-composition-api.component.html',
-  styleUrls: ['./directive-composition-api.component.css'],
+  template: `<app-header></app-header>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <div class="dropdown">
+            <button
+              class="btn btn-primary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {{ color | titlecase }}
+            </button>
+            <ul class="dropdown-menu">
+              <li *ngFor="let eachColor of colorList">
+                <button
+                  class="dropdown-item"
+                  (click)="setTheColorValue(eachColor)"
+                  type="button"
+                >
+                  {{ eachColor | titlecase }}
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-6 border">
+          <p class="mt-3" appStyle [myColor]="color" [myClass]="myFontClass">
+            Directive Composition API works!
+          </p>
+        </div>
+        <div class="col">
+          <div class="dropdown">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {{ myFontClass | titlecase }}
+            </button>
+            <ul class="dropdown-menu">
+              <li *ngFor="let eachFontClass of fontClassList">
+                <button
+                  class="dropdown-item"
+                  (click)="setTheFontValue(eachFontClass)"
+                  type="button"
+                >
+                  {{ eachFontClass | titlecase }}
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div> `,
   imports: [CommonModule, StyleDirective, HeaderComponent, TitleCasePipe],
 })
 export class DirectiveCompositionAPIComponent {

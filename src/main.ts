33c5bc015255daea477d2651835db6b5838d6df6
Jeevan-Mahesha <csejeevanm@gmail.com>
2023-03-17 15:10:57 +0530

    removed the routing and imported the BrowserAnimationsModule for global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MealFormComponent } from './app/meal-form/meal-form.component';
 
 bootstrapApplication(MealFormComponent, {
-  providers: [importProvidersFrom(AppRoutingModule)],
+  providers: [importProvidersFrom(BrowserAnimationsModule)],
 });

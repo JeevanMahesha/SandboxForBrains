6c1d365ea74571f5ef46b62fbcbbc9e01f1a04f7
Jeevan-Mahesha <csejeevanm@gmail.com>
2023-03-17 15:26:59 +0530

    AppRoutingModule added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
 
 import { MealFormComponent } from './app/meal-form/meal-form.component';
 
 bootstrapApplication(MealFormComponent, {
-  providers: [importProvidersFrom(BrowserAnimationsModule)],
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(AppRoutingModule),
+  ],
 });

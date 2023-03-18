3fb802a63144d6a7e3e3a272f37e06b604269a0c
Jeevan-Mahesha <csejeevanm@gmail.com>
2023-03-18 20:35:02 +0530

    totalMealDetails logic moved to dbaccess class

--- src/app/total/total.component.ts
+++ src/app/total/total.component.ts
@@ -2,22 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DbAccess } from '../DB/DB.access';
 import { HeaderComponent } from '../header/header.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
 import { IMeal } from '../meal-form/meal-form.model';
 import { ITotal } from './total.model';
 
 @Component({
   selector: 'app-total',
   standalone: true,
-  imports: [
-    CommonModule,
-    HeaderComponent,
-    MatDatepickerModule,
-    MatInputModule,
-    MatNativeDateModule,
-  ],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './total.component.html',
   styleUrls: ['./total.component.css'],
   providers: [DbAccess],
@@ -30,18 +21,6 @@ export class TotalComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     const allData = await this._db.getAllRecords();
     this.allMealDetails = allData.result;
-    console.log(this.allMealDetails);
-
-    this.allMealDetails.forEach(
-      ({ mealDate, mealTime, mealsConsumptionArray }) => {
-        const totalEachDate = mealsConsumptionArray.map((eachUser) => ({
-          ...eachUser,
-          mealDate,
-          mealTime,
-        }));
-        this.totalMealDetails.push(...totalEachDate);
-      }
-    );
-    console.log(this.totalMealDetails);
+    this.totalMealDetails = this._db.restructureTheData(this.allMealDetails);
   }
 }

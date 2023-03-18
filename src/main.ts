730a8fff01ae284a0b6cb79c19b7506c922a3066
Jeevan-Mahesha <csejeevanm@gmail.com>
2023-03-18 18:14:57 +0530

    ngx-toastr config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
@@ -8,5 +9,11 @@ bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserAnimationsModule),
     importProvidersFrom(AppRoutingModule),
+    importProvidersFrom(
+      ToastrModule.forRoot({
+        positionClass: 'toast-top-center',
+        closeButton: true,
+      })
+    ),
   ],
 });

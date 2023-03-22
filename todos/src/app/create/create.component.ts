6f28703f20c5e20077d20713220942147adadbce
Jeevan <csejeevanm@gmail.com>
2023-03-22 22:20:07 +0530

    angular 14 Updated

--- todos/src/app/create/create.component.ts
+++ todos/src/app/create/create.component.ts
@@ -1,35 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { DataPointService } from '../data-point.service';
+import { Component } from "@angular/core";
+import { UntypedFormBuilder, Validators } from "@angular/forms";
+import { DataPointService } from "../data-point.service";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.css"],
 })
-export class CreateComponent implements OnInit {
-
+export class CreateComponent {
   newData = this.reactiveForm.group({
-    'userId':['',Validators.required],
-    'title':['',Validators.required],
-    'body':['',Validators.required]
-  })
-  eachItem:any
+    userId: ["", Validators.required],
+    title: ["", Validators.required],
+    body: ["", Validators.required],
+  });
+  eachItem: any;
   constructor(
-    private reactiveForm:FormBuilder,
-    private postCall:DataPointService
-    ) { }
-
-  ngOnInit(): void {
-  }
-
-  submitNewData(){
+    private reactiveForm: UntypedFormBuilder,
+    private postCall: DataPointService
+  ) {}
+  submitNewData() {
     // console.log(this.newData.value);
-    this.postCall.createTodos(this.newData.value).subscribe(res=>{
+    this.postCall.createTodos(this.newData.value).subscribe((res) => {
       console.log(res);
-      this.eachItem = res
-    })
-    
+      this.eachItem = res;
+    });
   }
-
 }
--- todos/src/app/edit-todos/edit-todos.component.ts
+++ todos/src/app/edit-todos/edit-todos.component.ts
@@ -1,45 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-import { DataPointService } from '../data-point.service';
+import { Component, OnInit } from "@angular/core";
+import { UntypedFormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { DataPointService } from "../data-point.service";
 
 @Component({
-  selector: 'app-edit-todos',
-  templateUrl: './edit-todos.component.html',
-  styleUrls: ['./edit-todos.component.css']
+  selector: "app-edit-todos",
+  templateUrl: "./edit-todos.component.html",
+  styleUrls: ["./edit-todos.component.css"],
 })
 export class EditTodosComponent implements OnInit {
-
-  data:any
-  newEditData:any
-  dataSave:boolean=false
+  data: any;
+  newEditData: any;
+  dataSave: boolean = false;
   constructor(
-    private dataPoint:DataPointService,
-    private router:Router,
-    private reactiveForm:FormBuilder
+    private dataPoint: DataPointService,
+    private router: Router,
+    private reactiveForm: UntypedFormBuilder
   ) {
-    this.data = this.dataPoint.editIndex
+    this.data = this.dataPoint.editIndex;
     console.log(this.dataPoint, this.data);
   }
 
   ngOnInit(): void {
-    if(!this.dataPoint.editIndex.showData){
-      setTimeout(()=>{
-        this.router.navigate(['list'])
-      },5000)
-    }else{
-     this.newEditData = this.reactiveForm.group(this.dataPoint.listOfTodos[this.data?.index]) 
+    if (!this.dataPoint.editIndex.showData) {
+      setTimeout(() => {
+        this.router.navigate(["list"]);
+      }, 5000);
+    } else {
+      this.newEditData = this.reactiveForm.group(
+        this.dataPoint.listOfTodos[this.data?.index]
+      );
     }
   }
 
-
-  saveEdit(){
-   
-    this.dataPoint.listOfTodos[this.data.index] = JSON.parse(JSON.stringify(this.newEditData.value));
-    this.dataSave=true
-    setTimeout(()=>{
-      this.router.navigate(['list'])
-    },3000)
+  saveEdit() {
+    this.dataPoint.listOfTodos[this.data.index] = JSON.parse(
+      JSON.stringify(this.newEditData.value)
+    );
+    this.dataSave = true;
+    setTimeout(() => {
+      this.router.navigate(["list"]);
+    }, 3000);
   }
-
 }

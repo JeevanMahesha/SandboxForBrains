1760298ab70ba79e81a6e52c5ac906c213f4112f
Jeevan-Mahesha <csejeevanm@gmail.com>
2023-04-03 22:37:20 +0530

    delete component refactored

--- src/app/delete-record/delete-record.component.ts
+++ src/app/delete-record/delete-record.component.ts
@@ -1,64 +1,48 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
-import { MatInputModule } from '@angular/material/input';
+import { Component } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 import { environmentValues } from 'src/environment/environment';
-import { DbAccess } from '../DB/DB.access';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MealsConsumed } from '../app.model';
 
 @Component({
   selector: 'app-delete-record',
   standalone: true,
-  imports: [
-    CommonModule,
-    HeaderComponent,
-    MatInputModule,
-    ReactiveFormsModule,
-    ToastrModule,
-    MatProgressSpinnerModule,
-  ],
+  imports: [CommonModule, MatInputModule, ReactiveFormsModule],
   template: `
-    <app-header></app-header>
-    <mat-spinner class="spinner" *ngIf="pageLoading"></mat-spinner>
-
-    <div class="container-fluid text-center mt-5" *ngIf="!pageLoading">
-      <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-          <mat-form-field>
-            <mat-label>Password</mat-label>
-            <input
-              matInput
-              placeholder="Password"
-              [formControl]="password"
-              type="text"
-            />
-          </mat-form-field>
-          <br />
-          <button
-            [disabled]="password.invalid"
-            (click)="deleteAllRecords()"
-            class="btn btn-danger"
-          >
-            Delete all records
-          </button>
-        </div>
-        <div class="col-md-4"></div>
-      </div>
+    <div class="text-center mt-2 mb-2">
+      <mat-form-field>
+        <mat-label>Password</mat-label>
+        <input
+          matInput
+          placeholder="Password"
+          [formControl]="password"
+          type="password"
+        />
+      </mat-form-field>
+      <br />
+      <button class="btn btn-secondary" (click)="closeDialog('no')">
+        Close
+      </button>
+      &nbsp;
+      <button
+        *ngIf="!password.invalid"
+        (click)="closeDialog('yes')"
+        class="btn btn-danger"
+      >
+        Delete all records
+      </button>
     </div>
   `,
-  providers: [DbAccess],
 })
 export class DeleteRecordComponent {
-  pageLoading = false;
-  constructor(private _db: DbAccess, private toaster: ToastrService) {}
+  constructor(private dialogRef: MatDialogRef<DeleteRecordComponent>) {}
 
   password = new FormControl(null, [
     Validators.required,
@@ -74,29 +58,7 @@ export class DeleteRecordComponent {
       : { error: true };
   }
 
-  async deleteAllRecords(): Promise<void> {
-    this.pageLoading = true;
-    const allData = await this._db.getAllRecords();
-    if (!allData.result.length) {
-      this.toaster.info('No Records to Delete');
-      this.pageLoading = false;
-      return;
-    }
-    this._db
-      .deleteAllRecords()
-      .then((res) => {
-        if (res.result.deletedCount) {
-          this.toaster.success(
-            `${res.result.deletedCount} records are Deleted Successfully`
-          );
-          this.pageLoading = false;
-        }
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error);
-          this.toaster.error('Something went Wrong');
-        }
-      });
+  closeDialog(closeType: `${MealsConsumed}`) {
+    this.dialogRef.close(closeType);
   }
 }

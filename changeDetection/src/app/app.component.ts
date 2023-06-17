e25c96662c999d2b940d9a7f9350f9692789868a
Jeevan Mahesha <csejeevanm@gmail.com>
2023-06-17 20:33:45 +0530

    ChangeDetectionStrategy

--- changeDetection/src/app/app.component.ts
+++ changeDetection/src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'changeDetection';
+  list: any;
+  private httpClient = inject(HttpClient);
+  private cdr = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    this.httpClient
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((res) => {
+        this.cdr.markForCheck();
+        this.list = res;
+      });
+  }
 }

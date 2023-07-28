5d44ff9cf7a4f6f48da458acd71a343e1a18c0ae
Jeevan <34814862+JeevanMahesha@users.noreply.github.com>
2023-07-28 06:52:30 +0000

    Reading Data From The Store

--- NgRx/src/app/counter-output/counter-output.component.ts
+++ NgRx/src/app/counter-output/counter-output.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-
-import { CounterService } from '../counter.service';
+import { Component, inject } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-counter-output',
-  templateUrl: './counter-output.component.html',
-  styleUrls: ['./counter-output.component.css'],
+  selector: "app-counter-output",
+  templateUrl: "./counter-output.component.html",
+  styleUrls: ["./counter-output.component.css"],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter = 0;
-  counterServiceSub?: Subscription;
+export class CounterOutputComponent {
+  counter$: Observable<number> | null = null;
+  private store = inject(Store<{ counter: number }>);
+  constructor() {
+    this.counter$ = this.store.select("counter");
 
-  constructor(private counterService: CounterService) {}
-
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newVal) => (this.counter = newVal)
-    );
-  }
-
-  ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
   }
 }

35ab36daa20fa56cf1b760853529f27494cde6cc
Jeevan <csejeevanm@gmail.com>
2023-07-31 10:16:10 +0530

    Exploring Selectors

--- NgRx/src/app/counter-output/counter-output.component.ts
+++ NgRx/src/app/counter-output/counter-output.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
+import { doubleSelectCount, selectCount } from "../store/counter.select";
 
 @Component({
   selector: "app-counter-output",
@@ -9,8 +10,10 @@ import { Observable } from "rxjs";
 })
 export class CounterOutputComponent {
   counter$: Observable<number> | null = null;
+  doubleCounter$: Observable<number> | null = null;
   private store = inject(Store<{ counter: number }>);
   constructor() {
-    this.counter$ = this.store.select("counter");
+    this.counter$ = this.store.select(selectCount);
+    this.doubleCounter$ = this.store.select(doubleSelectCount)
   }
 }

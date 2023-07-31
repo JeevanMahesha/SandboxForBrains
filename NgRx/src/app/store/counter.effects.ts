eaa18cc574b6fe1105fe366834f522478abecd57
Jeevan <csejeevanm@gmail.com>
2023-07-31 11:16:04 +0530

    Using Store Data In Effects

--- NgRx/src/app/store/counter.effects.ts
+++ NgRx/src/app/store/counter.effects.ts
@@ -1,14 +1,24 @@
 import { Injectable, inject } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrementAction, incrementAction } from "./counter.actions";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
+import { Store } from "@ngrx/store";
+import { selectCount } from "./counter.select";
 
 @Injectable()
 export class CounterEffects {
     actions$ = inject(Actions)
+    store = inject(Store<{ counter: number }>)
     saveCount = createEffect(() => this.actions$.pipe(
         ofType(incrementAction, decrementAction),
-        tap(console.log)
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
+            localStorage.setItem('counter', counter.toString())
+            // action is initial value in store
+            // counter is current value 
+            console.log(action, counter);
+
+        })
     ),
         {
             dispatch: false

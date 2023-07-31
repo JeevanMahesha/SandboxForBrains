5d26259d829ed7a64aefa9be31aa85347811e804
Jeevan <csejeevanm@gmail.com>
2023-07-31 10:52:21 +0530

    Defining a First Effect

---
+++ NgRx/src/app/store/counter.effects.ts
@@ -0,0 +1,16 @@
+import { inject } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { decrementAction, incrementAction } from "./counter.actions";
+import { tap } from "rxjs";
+
+export class CounterEffects {
+    actions$ = inject(Actions)
+    saveCount = createEffect(() => this.actions$.pipe(
+        ofType(incrementAction, decrementAction),
+        tap(console.log)
+    ),
+        {
+            dispatch: false
+        }
+    );
+}

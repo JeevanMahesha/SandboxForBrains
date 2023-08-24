466cfb1988e0f0285cffe1da89bef9ef620e91cc
Jeevan <csejeevanm@gmail.com>
2023-08-24 12:45:44 +0530

    Optional decorator in dependency injection

--- DependencyInjection/src/app/app.component.ts
+++ DependencyInjection/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { LoggerService } from './logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DependencyInjection';
+
+  /*
+  Parameter decorator marks dependency as optional
+
+  if the dependency is not injected it will not throw null injector error no provider for service
+
+   constructor(@Optional() private lg: LoggerService) {
+    if (lg) {
+      lg.log('LoggerService');
+    }
+  }
+  */
 }

7f82771157f8427da9573d89e14bbd5feab6162c
Jeevan Mahesha <csejeevanm@gmail.com>
2023-09-24 18:19:14 +0530

    viewProviders

--- ReusableReactiveForm/src/app/address/address.component.ts
+++ ReusableReactiveForm/src/app/address/address.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  ControlContainer,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-address',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(ControlContainer, { skipSelf: true }),
+    },
+  ],
   templateUrl: './address.component.html',
   styleUrls: ['./address.component.css'],
 })
-export class AddressComponent {}
+export class AddressComponent implements OnInit {
+  private readonly parentControl = inject(ControlContainer);
+
+  get getAddressFormControlGroup() {
+    return this.parentControl.control as FormGroup;
+  }
+
+  ngOnInit() {
+    this.getAddressFormControlGroup.addControl(
+      'address',
+      new FormGroup({
+        country: new FormControl<string | null>('Jeevan'),
+        state: new FormControl<string | null>('Jeevan'),
+      })
+    );
+    console.log(this.getAddressFormControlGroup);
+  }
+}

c56292a3bc5a144c5c582e9dbd41d00de3962dee
Jeevan Mahesha <csejeevanm@gmail.com>
2023-10-22 12:59:18 +0530

    if statement

--- A17/src/app/app.component.ts
+++ A17/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IfComponent } from './if/if.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IfComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'A17';

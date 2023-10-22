60e735a970a8ccd5ce0a6e9a88141158e097c323
Jeevan Mahesha <csejeevanm@gmail.com>
2023-10-22 13:08:26 +0530

    for statement

--- A17/src/app/app.component.ts
+++ A17/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { IfComponent } from './if/if.component';
+import { ForComponent } from './for/for.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, IfComponent],
+  imports: [CommonModule, RouterOutlet, IfComponent, ForComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })

32f6cde3091da090ea1a46b78702395c06f5eaaf
Jeevan Mahesha <csejeevanm@gmail.com>
2023-10-24 13:25:10 +0530

    Track By Prop

--- ngxtension/src/app/track/track.component.ts
+++ ngxtension/src/app/track/track.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { TrackById } from 'ngxtension/trackby-id-prop';
+import { TrackById, TrackByProp } from 'ngxtension/trackby-id-prop';
 
 @Component({
   selector: 'app-track',
   standalone: true,
-  imports: [CommonModule, TrackById],
+  imports: [CommonModule, TrackById, TrackByProp],
   templateUrl: './track.component.html',
   styleUrls: ['./track.component.css'],
 })
@@ -16,11 +16,27 @@ export class TrackComponent {
     { id: 3, name: 'billy' },
   ];
 
+  trackByPropArray = [
+    { name: 'John Doe', age: 30, Dob: '1993-01-01' },
+    { name: 'Jane Doe', age: 25, Dob: '1998-02-02' },
+    { name: 'Peter Parker', age: 20, Dob: '2003-03-03' },
+    { name: 'Mary Jane Watson', age: 19, Dob: '2004-04-04' },
+  ];
+
   updateTrackByIdArray() {
     this.trackByIdArray = [
       { id: 1, name: 'foo' },
       { id: 2, name: 'bob' },
       { id: 3, name: 'billy' },
+    ];
+  }
+
+  updateTrackByPropArray() {
+    this.trackByPropArray = [
+      { name: 'John Doe', age: 30, Dob: '1993-01-01' },
+      { name: 'Jeevan Mahesha', age: 25, Dob: '1998-02-02' },
+      { name: 'Peter Parker', age: 20, Dob: '2003-03-03' },
+      { name: 'Mary Jane Watson', age: 19, Dob: '2004-04-04' },
     ];
   }
 }

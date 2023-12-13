4a2fae560dde065b255cc35800841db9c20a6c75
Jeevan Mahesha <csejeevanm@gmail.com>
2023-12-13 22:42:16 +0530

    material updated

--- materialV14/src/app/app-routing.module.ts
+++ materialV14/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../app/app.module').then((m) => m.AppModule),
+  },
+  {
+    path: 'mv15',
+    loadChildren: () =>
+      import('../app/material-v15-comp/material-v15-comp.module').then(
+        (m) => m.MaterialV15CompModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- materialV14/src/app/material-v15-comp/material-v15-comp.module.ts
+++ materialV14/src/app/material-v15-comp/material-v15-comp.module.ts
@@ -3,11 +3,10 @@ import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/
 
 import { CommonModule } from '@angular/common';
 import { MV15Component } from './m-v15/m-v15.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    MV15Component
-  ],
-  imports: [CommonModule, MatCheckboxModule],
+  declarations: [MV15Component],
+  imports: [CommonModule, MatCheckboxModule, RouterModule],
 })
 export class MaterialV15CompModule {}

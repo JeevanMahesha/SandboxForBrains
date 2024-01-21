80e6743cddc706d01eb9a45bfd3fbbb4a3576c48
Jeevan Mahesha <csejeevanm@gmail.com>
2024-01-21 18:19:22 +0530

    UI update

--- SignalInputs/src/app/app.component.ts
+++ SignalInputs/src/app/app.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
 import { DecoratorInputComponent } from './decorator-input/decorator-input.component';
 import { SignalInputComponent } from './signal-input/signal-input.component';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DecoratorInputComponent, SignalInputComponent],
+  imports: [DecoratorInputComponent, SignalInputComponent, TitleCasePipe],
   template: `
-    <div>
-      <app-decorator-input
-        [countValue]="initialCountValue"
-        [doubleCountValue]="initialCountValue"
-      ></app-decorator-input>
-      <app-signal-input
-        [countValue]="initialCountValue"
-        [doubleCountValue]="initialCountValue"
-      ></app-signal-input>
-      <button type="button" (click)="increaseCount()">Increase Count</button>
+    <div class="container text-center mt-5 ">
+      <div class="row">
+        <div class="col border">
+          <h4>{{ 'decorator input' | titlecase }}</h4>
+          <app-decorator-input
+            [countValue]="initialCountValue"
+            [doubleCountValue]="initialCountValue"
+          ></app-decorator-input>
+        </div>
+        <div class="col border">
+          <h4>{{ 'signal input' | titlecase }}</h4>
+
+          <app-signal-input
+            [countValue]="initialCountValue"
+            [doubleCountValue]="initialCountValue"
+          ></app-signal-input>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col"></div>
+        <div class="col mt-2">
+          <button
+            type="button"
+            class="btn btn-primary"
+            (click)="increaseCount()"
+          >
+            Increase Count
+          </button>
+        </div>
+        <div class="col"></div>
+      </div>
     </div>
   `,
 })

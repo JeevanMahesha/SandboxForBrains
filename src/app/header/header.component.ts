8239c5936624639e844f5b1d696d3ae842db285b
Jeevan Mahesha <csejeevanm@gmail.com>
2024-06-09 20:04:09 +0530

    updated the name component with new implementation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { RouterModule } from '@angular/router';
-import { take } from 'rxjs';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, take } from 'rxjs';
 import { DbAccess } from '../DB/DB.access';
 import { MealsConsumed } from '../app.model';
 import { DeleteRecordComponent } from '../delete-record/delete-record.component';
-import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    CommonModule,
-    RouterModule,
+    NgClass,
+    RouterLinkActive,
+    RouterLink,
     MatDialogModule,
     DeleteRecordComponent,
     MatIcon,
@@ -36,8 +37,7 @@ import { MatIcon } from '@angular/material/icon';
             >All Meal Records</a
           >
           <a
-            [routerLink]="['/delete-records']"
-            routerLinkActive="active"
+            (click)="deleteAllRecords()"
             class="text-white hover:bg-white hover:text-gray-900 px-3 py-2 rounded-md"
             >Delete Records</a
           >
@@ -81,8 +81,7 @@ import { MatIcon } from '@angular/material/icon';
           >All Meal Records</a
         >
         <a
-          [routerLink]="['/delete-records']"
-          routerLinkActive="active"
+          (click)="deleteAllRecords()"
           class="text-white hover:bg-white hover:text-gray-900 px-3 py-2 rounded-md"
           >Delete Records</a
         >
@@ -96,88 +95,6 @@ import { MatIcon } from '@angular/material/icon';
           [routerLink]="['/report']"
           routerLinkActive="active"
           class="text-white hover:bg-white hover:text-gray-900 px-3 py-2 rounded-md"
-          >Report</a
-        >
-      </div>
-    </nav>
-
-    <nav *ngIf="false" class="bg-white p-4 rounded mb-4 shadow-md">
-      <div class="container mx-auto flex justify-between items-center">
-        <div class="text-gray-800 font-bold text-xl">Meal Tracker</div>
-        <div class="hidden md:flex space-x-4">
-          <a
-            [routerLink]="['/meal-form']"
-            routerLinkActive="active"
-            class="text-gray-800 hover:bg-gray-400 px-3 py-2 rounded-md"
-            >Meal</a
-          >
-          <a
-            [routerLink]="['/all']"
-            routerLinkActive="active"
-            class="text-gray-800 hover:bg-gray-400 px-3 py-2 rounded-md"
-            >All Meal Records</a
-          >
-          <a
-            [routerLink]="['/delete-records']"
-            routerLinkActive="active"
-            class="text-gray-800 hover:bg-gray-400 px-3 py-2 rounded-md"
-            >Delete Records</a
-          >
-          <a
-            [routerLink]="['/detail-view']"
-            routerLinkActive="active"
-            class="text-gray-800 hover:bg-gray-400 px-3 py-2 rounded-md"
-            >Detail</a
-          >
-          <a
-            [routerLink]="['/report']"
-            routerLinkActive="active"
-            class="text-gray-800 hover:bg-gray-400 px-3 py-2 rounded-md"
-            >Report</a
-          >
-        </div>
-        <div class="md:hidden flex items-center">
-          <button
-            mat-icon-button
-            (click)="menuOpen = !menuOpen"
-            class="text-gray-800"
-          >
-            <mat-icon>menu</mat-icon>
-          </button>
-        </div>
-      </div>
-      <div
-        [ngClass]="{ block: menuOpen, hidden: !menuOpen }"
-        class="md:hidden mt-4"
-      >
-        <a
-          [routerLink]="['/meal-form']"
-          routerLinkActive="active"
-          class="text-gray-800 block w-full text-left px-3 py-2 rounded-md"
-          >Meal</a
-        >
-        <a
-          [routerLink]="['/all']"
-          routerLinkActive="active"
-          class="text-gray-800 block w-full text-left px-3 py-2 rounded-md"
-          >All Meal Records</a
-        >
-        <a
-          [routerLink]="['/delete-records']"
-          routerLinkActive="active"
-          class="text-gray-800 block w-full text-left px-3 py-2 rounded-md"
-          >Delete Records</a
-        >
-        <a
-          [routerLink]="['/detail-view']"
-          routerLinkActive="active"
-          class="text-gray-800 block w-full text-left px-3 py-2 rounded-md"
-          >Detail</a
-        >
-        <a
-          [routerLink]="['/report']"
-          routerLinkActive="active"
-          class="text-gray-800 block w-full text-left px-3 py-2 rounded-md"
           >Report</a
         >
       </div>
@@ -186,17 +103,16 @@ import { MatIcon } from '@angular/material/icon';
 })
 export class HeaderComponent {
   menuOpen = false;
-  constructor(public dialog: MatDialog, private _db: DbAccess) {}
-
+  dialog = inject(MatDialog);
+  private _db = inject(DbAccess);
   deleteAllRecords() {
     this.dialog
       .open(DeleteRecordComponent, { width: '500px', disableClose: true })
       .afterClosed()
-      .pipe(take(1))
-      .subscribe((res: keyof typeof MealsConsumed) => {
-        if (res === 'Yes') {
-          this._db.deleteAllRecords();
-        }
-      });
+      .pipe(
+        take(1),
+        filter((userAction: keyof typeof MealsConsumed) => userAction === 'Yes')
+      )
+      .subscribe(this._db.deleteAllRecords.bind(this._db));
   }
 }

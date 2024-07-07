a49442eb1a317bc6cf869875c1d37ef546eba8cc
Jeevan Mahesha <csejeevanm@gmail.com>
2024-07-07 13:45:04 +0530

    handling events

--- reactBasic/src/components/ListGroup.tsx
+++ reactBasic/src/components/ListGroup.tsx
@@ -14,13 +14,17 @@ function ListGroup() {
 
   const notFoundCountries = countries.length === 0 && <p>No countries found</p>;
 
+  const handleClick = (clickEvent: React.MouseEvent) => {
+    console.log("clicked", clickEvent);
+  };
+
   return (
     <>
       <h1>List Group</h1>
       {notFoundCountries}
       <ul className="list-group">
         {countries.map((country) => (
-          <li className="list-group-item" key={country}>
+          <li className="list-group-item" onClick={handleClick} key={country}>
             {country}
           </li>
         ))}

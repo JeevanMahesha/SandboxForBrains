0c8bbf946ddfa04d9be56508dfc98b89d5064a72
Jeevan Mahesha <csejeevanm@gmail.com>
2024-07-07 13:35:34 +0530

    conditions

--- reactBasic/src/components/ListGroup.tsx
+++ reactBasic/src/components/ListGroup.tsx
@@ -12,9 +12,12 @@ function ListGroup() {
     "India",
   ];
 
+  const notFoundCountries = countries.length === 0 && <p>No countries found</p>;
+
   return (
     <>
       <h1>List Group</h1>
+      {notFoundCountries}
       <ul className="list-group">
         {countries.map((country) => (
           <li className="list-group-item" key={country}>

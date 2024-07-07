d748a232508931a0c93cfc789e394d1a09e1921c
Jeevan Mahesha <csejeevanm@gmail.com>
2024-07-07 13:28:28 +0530

    loop the list

--- reactBasic/src/components/ListGroup.tsx
+++ reactBasic/src/components/ListGroup.tsx
@@ -1,13 +1,26 @@
 function ListGroup() {
+  const countries = [
+    "United States",
+    "Canada",
+    "Mexico",
+    "Brazil",
+    "United Kingdom",
+    "Germany",
+    "France",
+    "Italy",
+    "Australia",
+    "India",
+  ];
+
   return (
     <>
       <h1>List Group</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {countries.map((country) => (
+          <li className="list-group-item" key={country}>
+            {country}
+          </li>
+        ))}
       </ul>
     </>
   );

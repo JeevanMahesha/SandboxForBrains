8109475bcc2f063e8d4cd27eb072f53c2dad0cff
Jeevan Mahesha <csejeevanm@gmail.com>
2024-07-20 20:40:07 +0530

    observable and initial value added

--- angular18/src/app/zoneless/zoneless.component.ts
+++ angular18/src/app/zoneless/zoneless.component.ts
@@ -1,15 +1,18 @@
+import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-zoneless',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgTemplateOutlet],
   templateUrl: './zoneless.component.html',
   styleUrl: './zoneless.component.css',
 })
 export default class ZonelessComponent {
   plainCounter = 0;
   signalCounter = signal(0);
+  counter$: Observable<number> | null = null;
 
   incrementPlainCounter() {
     setInterval(() => this.plainCounter++, 1000);
@@ -17,5 +20,9 @@ export default class ZonelessComponent {
 
   incrementSignalCounter() {
     setInterval(() => this.signalCounter.set(this.signalCounter() + 1), 1000);
+  }
+
+  incrementCounter() {
+    this.counter$ = interval(1000);
   }
 }

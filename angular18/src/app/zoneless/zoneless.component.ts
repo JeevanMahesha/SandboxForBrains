85b4153a6ae802520cfc8ef018969ae229ae5f2c
Jeevan Mahesha <csejeevanm@gmail.com>
2024-07-20 20:19:38 +0530

    signal base counter

--- angular18/src/app/zoneless/zoneless.component.ts
+++ angular18/src/app/zoneless/zoneless.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, NgZone } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-zoneless',
@@ -9,8 +9,13 @@ import { Component, inject, NgZone } from '@angular/core';
 })
 export default class ZonelessComponent {
   plainCounter = 0;
+  signalCounter = signal(0);
 
   incrementPlainCounter() {
     setInterval(() => this.plainCounter++, 1000);
+  }
+
+  incrementSignalCounter() {
+    setInterval(() => this.signalCounter.set(this.signalCounter() + 1), 1000);
   }
 }

64c9fa9dcd6a7111ce1416386d232e3beb9081c2
Jeevan Mahesha <csejeevanm@gmail.com>
2024-07-22 22:22:43 +0530

    route path updated

--- angular18/src/app/app.component.ts
+++ angular18/src/app/app.component.ts
@@ -39,12 +39,6 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
                 >Signal</a
               >
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
           </ul>
         </div>
       </div>

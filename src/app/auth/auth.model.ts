aca6d9d2265655d4f3c39e582150db8411ad1e89
Jeevan Mahesha <csejeevanm@gmail.com>
2024-09-17 22:34:59 +0530

    User profile data added

---
+++ src/app/auth/auth.model.ts
@@ -0,0 +1,10 @@
+export interface IUserProfile {
+  name: string;
+  granted_scopes: string;
+  id: string;
+  verified_email: boolean;
+  given_name: string;
+  family_name: string;
+  email: string;
+  picture: string;
+}

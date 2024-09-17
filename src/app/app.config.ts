2ed866940046360364af091a9511ab72a468eaf3
Jeevan Mahesha <csejeevanm@gmail.com>
2024-09-17 21:38:25 +0530

    fireBase installed and added the initial config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { fireBaseInitializeAppConfig } from '../environment/env.dev';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: fireBaseInitializeAppConfig.projectId,
+        appId: fireBaseInitializeAppConfig.appId,
+        storageBucket: fireBaseInitializeAppConfig.storageBucket,
+        apiKey: fireBaseInitializeAppConfig.apiKey,
+        authDomain: fireBaseInitializeAppConfig.authDomain,
+        messagingSenderId: fireBaseInitializeAppConfig.messagingSenderId,
+        measurementId: fireBaseInitializeAppConfig.measurementId,
+      })
+    ),
+    provideAuth(() => getAuth()),
+  ],
 };

a8329bbe3f18ec12a6e5b076365823ecc3bcd0e1
Jeevan Mahesha <csejeevanm@gmail.com>
2024-09-19 22:44:28 +0530

    Login Ui and action finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './auth/auth.service';
@@ -7,11 +6,19 @@ import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, CommonModule],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Rooted-Baskets';
   #authService = inject(AuthService);
+
+  get isUserLoggedIn() {
+    return !!this.#authService.loggedInUserDetail;
+  }
+
+  login() {
+    this.#authService.signInWithGoogle();
+  }
 }

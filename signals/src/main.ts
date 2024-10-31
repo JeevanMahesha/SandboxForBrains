6aa25f7041a264b098bd0cb4134337fca625d3fd
Jeevan Mahesha <csejeevanm@gmail.com>
2024-10-31 13:25:43 +0530

    refactor: simplify provider imports in main.ts

--- signals/src/main.ts
+++ signals/src/main.ts
@@ -1,14 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule)]
-})
-  .catch(err => console.error(err));
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule)],
+}).catch((err) => console.error(err));

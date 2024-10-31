88d44b6658ef9b1aa551d1575ba7f031bcf48b32
Jeevan Mahesha <csejeevanm@gmail.com>
2024-10-31 21:34:36 +0530

    feat: implement Angular Material tabs in AppComponent for enhanced UI

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <section class="p-5">
+      <h1 class="text-center">Angular Signals</h1>
+      <mat-tab-group>
+        <mat-tab label="First"> Content 1 </mat-tab>
+      </mat-tab-group>
+    </section>
+  `,
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
 })
 export class AppComponent {}

df8139a77bde44bcd790f655166135b9065cb06a
Jeevan Mahesha <csejeevanm@gmail.com>
2024-11-01 10:34:19 +0530

    feat: enhance SignalComponent with user detail inputs and update functionality

--- signals/src/app/signal/signal.component.ts
+++ signals/src/app/signal/signal.component.ts
@@ -1,13 +1,26 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signal.component.html',
   standalone: true,
 })
 export class SignalComponent {
   userName = signal('Jeevan Mahesha');
+  userDetailNameValue: string | null = null;
+  userDetailNumberValue: number | null = null;
+
+  userDetail = signal<IUser>(
+    {
+      name: 'Jeevan Mahesha',
+      userId: 1,
+    },
+    {
+      equal: (prev, next) => prev.userId === next.userId,
+    }
+  );
 
   setUserName(userName: string) {
     this.userName.set(userName);
@@ -16,4 +29,29 @@ export class SignalComponent {
   updateUserName(value: string) {
     this.userName.update((prev) => value.concat(prev));
   }
+
+  updateUserDetailName() {
+    if (!this.userDetailNameValue) {
+      return;
+    }
+    this.userDetail.update((prev) => ({
+      ...prev,
+      name: this.userDetailNameValue!,
+    }));
+  }
+
+  updateUserDetailNumber() {
+    if (!this.userDetailNumberValue) {
+      return;
+    }
+    this.userDetail.update((prev) => ({
+      ...prev,
+      userId: this.userDetailNumberValue!,
+    }));
+  }
+}
+
+interface IUser {
+  name: string;
+  userId: number;
 }

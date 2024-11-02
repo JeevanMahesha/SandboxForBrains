3fff8e75ff5ee0838129814377bdfcdb9a617698
Jeevan Mahesha <csejeevanm@gmail.com>
2024-11-02 15:32:36 +0530

    feat: add CustomToggleComponent and ModelInputsComponent with initial templates and tests

---
+++ signals/src/app/model-inputs/custom-toggle/custom-toggle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-toggle',
+  imports: [],
+  standalone: true,
+  templateUrl: './custom-toggle.component.html',
+  styleUrl: './custom-toggle.component.css',
+})
+export class CustomToggleComponent {}
---
+++ signals/src/app/model-inputs/model-inputs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-inputs',
+  imports: [],
+  standalone: true,
+  templateUrl: './model-inputs.component.html',
+  styleUrl: './model-inputs.component.css',
+})
+export class ModelInputsComponent {}

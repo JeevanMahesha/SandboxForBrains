2d4c78251dac58598c7d1c2055ce46042c371873
Jeevan Mahesha <csejeevanm@gmail.com>
2024-11-02 12:22:17 +0530

    feat: add ToSignalComponent and integrate into AppComponent with lazy loading

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { SignalComponent } from './signal.component';
 import { ComputedSignalComponent } from './computed-signal.component';
+import { ToSignalComponent } from './to-signal/to-signal.component';
 
 @Component({
   selector: 'app-root',
@@ -23,10 +24,22 @@ import { ComputedSignalComponent } from './computed-signal.component';
           <h6>Loading the Computed signal Component</h6>
           }
         </mat-tab>
+        <mat-tab label="To signal">
+          @defer (on viewport) {
+          <app-to-signal />
+          } @placeholder (minimum 500ms) {
+          <h6>Loading the To signal Component</h6>
+          }
+        </mat-tab>
       </mat-tab-group>
     </section>
   `,
   standalone: true,
-  imports: [MatTabsModule, SignalComponent, ComputedSignalComponent],
+  imports: [
+    MatTabsModule,
+    SignalComponent,
+    ComputedSignalComponent,
+    ToSignalComponent,
+  ],
 })
 export class AppComponent {}
---
+++ signals/src/app/to-signal/to-signal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-signal',
+  imports: [],
+  standalone: true,
+  templateUrl: './to-signal.component.html',
+  styleUrl: './to-signal.component.css',
+})
+export class ToSignalComponent {}

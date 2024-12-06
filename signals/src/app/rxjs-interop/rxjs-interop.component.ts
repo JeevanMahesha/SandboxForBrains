5704a8436cd61978b58b57b2c6fb68eb35d1298a
Jeevan Mahesha <csejeevanm@gmail.com>
2024-12-06 12:25:21 +0530

    feat: add ToObservableComponent and integrate with RxjsInteropComponent

--- signals/src/app/rxjs-interop/rxjs-interop.component.ts
+++ signals/src/app/rxjs-interop/rxjs-interop.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ToSignalComponent } from './to-signal.component';
+import { ToObservableComponent } from './to-observable.component';
 
 @Component({
-    selector: 'app-rxjs-interop',
-    imports: [ToSignalComponent, MatExpansionModule],
-    templateUrl: './rxjs-interop.component.html',
-    styleUrl: './rxjs-interop.component.css'
+  selector: 'app-rxjs-interop',
+  imports: [ToSignalComponent, ToObservableComponent, MatExpansionModule],
+  templateUrl: './rxjs-interop.component.html',
+  styleUrl: './rxjs-interop.component.css',
 })
 export class RxjsInteropComponent {}

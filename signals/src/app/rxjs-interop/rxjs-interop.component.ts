7ced71239275ad1ae7c473976ee31f5818121d54
Jeevan Mahesha <csejeevanm@gmail.com>
2024-12-06 12:43:21 +0530

    feat: add RxResourceComponent for food filtering functionality

--- signals/src/app/rxjs-interop/rxjs-interop.component.ts
+++ signals/src/app/rxjs-interop/rxjs-interop.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ToSignalComponent } from './to-signal.component';
 import { ToObservableComponent } from './to-observable.component';
+import { RxResourceComponent } from './rx-resource.component';
 
 @Component({
   selector: 'app-rxjs-interop',
-  imports: [ToSignalComponent, ToObservableComponent, MatExpansionModule],
+  imports: [
+    ToSignalComponent,
+    ToObservableComponent,
+    RxResourceComponent,
+    MatExpansionModule,
+  ],
   templateUrl: './rxjs-interop.component.html',
   styleUrl: './rxjs-interop.component.css',
 })

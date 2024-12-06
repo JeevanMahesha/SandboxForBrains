9fffaec03ca4c2688342acd071aa7b8e79d459c3
Jeevan Mahesha <csejeevanm@gmail.com>
2024-12-06 10:37:41 +0530

    feat: implement loading state and error handling in ResourceComponent with dynamic table display

--- signals/src/app/resource/resource.component.ts
+++ signals/src/app/resource/resource.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, resource, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-resource',
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule],
+  standalone: true,
   templateUrl: './resource.component.html',
-  styleUrl: './resource.component.css'
+  styleUrl: './resource.component.css',
 })
 export class ResourceComponent {
+  limitDropDownValues = Array.from({ length: 10 }, (_, i) => i + 1);
+  selectedLimit = signal(5);
+  user = resource<ITodo[], number>({
+    request: this.selectedLimit,
+    loader: ({ request: limitValue }) =>
+      fetch(
+        'https://dummyjson.com/todos?limit='.concat(limitValue.toString())
+      ).then((res) => res.json().then((data) => data.todos)),
+  });
+}
 
+export interface ITodo {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
 }

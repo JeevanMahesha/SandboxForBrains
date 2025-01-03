a4bf827da420d47ef51a0cf56b5e1583c9dcb0ae
Jeevan Mahesha <jeevan.mahesha@trustrace.com>
2025-01-03 13:58:50 +0530

    feat: add WithRxJs component and integrate into app; update tabs and remove unnecessary styles

--- ngrxSignals/src/app/app.component.ts
+++ ngrxSignals/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { TodoStore } from './with-out-rx-js/todo.store';
 import { WithOutRxJsComponent } from './with-out-rx-js/with-out-rx-js.component';
+import { WithRxJsComponent } from './with-rx-js/with-rx-js.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [MatTabsModule, WithOutRxJsComponent],
+  imports: [MatTabsModule, WithOutRxJsComponent, WithRxJsComponent],
 })
 export class AppComponent {
   title = 'ngrxSignals';
---
+++ ngrxSignals/src/app/with-rx-js/with-rx-js.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-with-rx-js',
+  imports: [],
+  templateUrl: './with-rx-js.component.html',
+  styleUrl: './with-rx-js.component.css',
+})
+export class WithRxJsComponent {}

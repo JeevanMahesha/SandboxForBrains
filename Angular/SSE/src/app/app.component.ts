20c1ed46a70e52c516cee459574c969564cfdc70
Jeevan Mahesha <csejeevanm@gmail.com>
2025-02-02 14:35:19 +0530

    Implement SSE client-side functionality in Angular

--- Angular/SSE/src/app/app.component.ts
+++ Angular/SSE/src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'SSE';
+
+  private readonly url = 'http://localhost:3000/';
+  private readonly http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe((data) => {
+      console.log(data);
+      this.getEvents();
+    });
+  }
+
+  private getEvents() {
+    const eventSource = new EventSource(this.url + 'events');
+    let count = 0;
+    eventSource.onmessage = (event) => {
+      count++;
+      console.log(event.data, count);
+      if (count > 10) {
+        eventSource.close();
+      }
+    };
+    eventSource.onerror = (error) => {
+      console.error('EventSource failed:', error);
+      eventSource.close();
+    };
+  }
 }

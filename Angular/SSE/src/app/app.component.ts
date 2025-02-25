fe0dc33316ab83182934d4865cf2763ecbbe1560
Jeevan Mahesha <csejeevanm@gmail.com>
2025-02-25 17:44:59 +0530

    Enhance countdown timer UI with live notifications and connection status

--- Angular/SSE/src/app/app.component.ts
+++ Angular/SSE/src/app/app.component.ts
@@ -1,41 +1,72 @@
-import { JsonPipe } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
 })
 export class AppComponent {
-  private readonly url = 'http://localhost:3000/';
   readonly notificationCount = signal(0);
-  // readonly eventSourceSignal = signal(new EventSource(this.url + 'events'));
 
-  // constructor() {
-  //   effect(() => {
-  //     if (this.notificationCount() > 0) {
-  //       this.initializeEventStream();
-  //     }
-  //   });
-  // }
+  eventSource: EventSource | null = null;
+
+  readonly connectionStatusClass = {
+    'bg-yellow-100 text-yellow-800':
+      this.eventSource?.readyState === EventSource.CONNECTING,
+    'bg-green-100 text-green-800':
+      this.eventSource?.readyState === EventSource.OPEN,
+    'bg-red-100 text-red-800':
+      this.eventSource?.readyState === EventSource.CLOSED,
+  };
+  readonly connectionStatusText: Record<number, string> = {
+    0: 'Connecting...',
+    1: 'Connected',
+    2: 'Disconnected',
+  };
+
+  readonly liveNotifications = signal<SSEResponse[]>([]);
+
+  constructor() {
+    effect(() => {
+      if (this.eventSource?.readyState === EventSource.OPEN) {
+        this.eventSource?.close();
+      }
+      if (this.notificationCount() > 0) {
+        this.initializeEventStream();
+        this.liveNotifications.set([]);
+      }
+    });
+  }
 
   private initializeEventStream() {
-    console.log('Initializing event stream');
-
-    // const eventSource = new EventSource(this.url + 'events');
-    // let count = 0;
-    // eventSource.onmessage = (notificationEvent) => {
-    //   count++;
-    //   console.log(notificationEvent.data, count);
-    //   if (count > this.notificationCount()) {
-    //     eventSource.close();
-    //   }
-    // };
-    // eventSource.onerror = (error) => {
-    //   console.error('EventSource failed:', error);
-    //   eventSource.close();
-    // };
+    this.eventSource = new EventSource('http://localhost:3000/events');
+    if (!this.eventSource) {
+      console.error('EventSource not supported');
+      return;
+    }
+    let count = 0;
+    this.eventSource.onmessage = (notificationEvent: MessageEvent<string>) => {
+      const sseResponse = JSON.parse(notificationEvent.data) as SSEResponse;
+      count++;
+      this.liveNotifications.update((notifications) => [
+        ...notifications,
+        sseResponse,
+      ]);
+      if (count >= this.notificationCount()) {
+        this.eventSource?.close();
+      }
+    };
+    this.eventSource.onerror = (error) => {
+      console.error('EventSource failed:', error);
+      this.eventSource?.close();
+    };
   }
+}
+
+interface SSEResponse {
+  message: string;
+  currentTime: string;
 }

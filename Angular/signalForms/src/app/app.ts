9cbe26b877e9fd48c58c2efb446d7af6b4ee9315
Jeevan Mahesha <csejeevanm@gmail.com>
2025-09-05 00:03:03 +0530

    feat: Enhance app structure with Bootstrap integration and form handling
    - Updated index.html to include Bootstrap CSS and JS for improved styling and functionality.
    - Refactored app.html to implement a responsive accordion form layout with various input fields, including required, optional, async validator, and disabled fields.
    - Removed placeholder content and integrated dynamic form control handling using Angular signals.
    - Deleted outdated app.spec.ts test file as part of the cleanup.

--- Angular/signalForms/src/app/app.ts
+++ Angular/signalForms/src/app/app.ts
@@ -1,12 +1,46 @@
+import { JsonPipe, NgTemplateOutlet } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Control, disabled, form, required, validateHttp } from '@angular/forms/signals';
+
+interface signalForms {
+  requiredField: string;
+  optionalField: string;
+  defaultValueField: string;
+  disabledField: string;
+  asyncValidatorField: string | null;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Control, JsonPipe, NgTemplateOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('signalForms');
+
+  public signalForm = form<signalForms>(
+    signal({
+      asyncValidatorField: null,
+      disabledField: '',
+      defaultValueField: 'CUSTOM DEFAULT VALUE',
+      optionalField: '',
+      requiredField: '',
+    }),
+    (currentControl) => {
+      required(currentControl.requiredField);
+      disabled(currentControl.disabledField, () => 'Reason for disabling this field');
+      validateHttp(currentControl.asyncValidatorField, {
+        request: ({ value }) => `https://dummyjson.com/users/search?q=${value()}`,
+        errors: (result) => {
+          const apiResult = result as unknown as { users: unknown[] };
+          return !apiResult.users.length ? [{ kind: 'notUnique' }] : undefined;
+        },
+      });
+    }
+  );
+
+  printFormValues() {
+    console.log(this.signalForm());
+  }
 }

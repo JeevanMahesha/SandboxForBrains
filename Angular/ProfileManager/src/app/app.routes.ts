771270857cfe0947f006f505aa8d99ed633608f3
Jeevan Mahesha <csejeevanm@gmail.com>
2026-03-28 19:53:16 +0530

    feat: restructure app to introduce v1 routing and component organization, removing legacy sidebar and integrating new layout

--- Angular/ProfileManager/src/app/app.routes.ts
+++ Angular/ProfileManager/src/app/app.routes.ts
@@ -8,23 +8,30 @@ export const routes: Routes = [
     canActivate: [loginGuard],
   },
   {
-    path: '',
-    loadComponent: () => import('./components/profiles-list/profiles-list'),
+    path: 'v1',
+    loadComponent: () => import('./v1/v1'),
     canActivate: [authGuard],
-  },
-  {
-    path: 'profile',
-    loadComponent: () => import('./components/add-profile/add-profile'),
-    canActivate: [authGuard],
-  },
-  {
-    path: 'matching-stars',
-    loadComponent: () => import('./components/matching-stars/matching-stars'),
-    canActivate: [authGuard],
-  },
-  {
-    path: 'zodiac-list',
-    loadComponent: () => import('./components/zodiac-list/zodiac-list'),
-    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./components/profiles-list/profiles-list'),
+        canActivate: [authGuard],
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./components/add-profile/add-profile'),
+        canActivate: [authGuard],
+      },
+      {
+        path: 'matching-stars',
+        loadComponent: () => import('./components/matching-stars/matching-stars'),
+        canActivate: [authGuard],
+      },
+      {
+        path: 'zodiac-list',
+        loadComponent: () => import('./components/zodiac-list/zodiac-list'),
+        canActivate: [authGuard],
+      },
+    ],
   },
 ];

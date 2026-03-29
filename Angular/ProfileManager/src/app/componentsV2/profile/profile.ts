549168890960ab76cbce9da3094df7d261d1eeb0
Jeevan Mahesha <csejeevanm@gmail.com>
2026-03-29 17:34:25 +0530

    feat: implement profile details form and activity timeline in drawer component

--- Angular/ProfileManager/src/app/componentsV2/profile/profile.ts
+++ Angular/ProfileManager/src/app/componentsV2/profile/profile.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { DrawerModule } from 'primeng/drawer';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { TimelineModule } from 'primeng/timeline';
 
 @Component({
   selector: 'app-profile',
-  imports: [DrawerModule],
+  imports: [
+    DrawerModule,
+    InputTextModule,
+    SelectModule,
+    FloatLabelModule,
+    InputMaskModule,
+    InputNumberModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ButtonModule,
+    TimelineModule,
+  ],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class Profile {
   isVisable = true;
+  events = [
+    {
+      status: 'Ordered',
+      date: '15/10/2020 10:30',
+      icon: 'pi pi-shopping-cart',
+      color: '#9C27B0',
+      image: 'game-controller.jpg',
+    },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+    { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
+    { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' },
+  ];
 }

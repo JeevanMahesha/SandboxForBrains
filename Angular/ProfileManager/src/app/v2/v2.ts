1a156887968cebe51d50b042894a0d59d1f5b3a3
Jeevan Mahesha <csejeevanm@gmail.com>
2026-03-29 15:32:00 +0530

    feat: implement TopBar component and integrate into V2 layout

--- Angular/ProfileManager/src/app/v2/v2.ts
+++ Angular/ProfileManager/src/app/v2/v2.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import ProfilesList from '../componentsV2/profiles-list/profiles-list';
+import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
+import { TopBar } from '../componentsV2/top-bar/top-bar';
 
 @Component({
   selector: 'app-v2',
-  imports: [ProfilesList],
+  imports: [DrawerModule, ButtonModule, TopBar],
   templateUrl: './v2.html',
   styleUrl: './v2.css',
 })
-export default class V2 {}
+export default class V2 {
+  sidebarVisible = true;
+}

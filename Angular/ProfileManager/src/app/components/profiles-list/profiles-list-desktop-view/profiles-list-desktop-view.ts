a6998b28be887f8867bf954538bef7d7ab1243bf
Jeevan Mahesha <csejeevanm@gmail.com>
2026-04-03 18:33:56 +0530

    refactor: enhance profile input fields with float labels and improve profile status display in lists for better user experience

--- Angular/ProfileManager/src/app/components/profiles-list/profiles-list-desktop-view/profiles-list-desktop-view.ts
+++ Angular/ProfileManager/src/app/components/profiles-list/profiles-list-desktop-view/profiles-list-desktop-view.ts
@@ -1,3 +1,4 @@
+import { TitleCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
@@ -8,14 +9,13 @@ import { ProfilesService } from '../../../services/profiles.service';
 
 @Component({
   selector: 'app-profiles-list-desktop-view',
-  imports: [TableModule, ButtonModule, TagModule],
+  imports: [TableModule, ButtonModule, TagModule, TitleCasePipe],
   templateUrl: './profiles-list-desktop-view.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfilesListDesktopView {
   readonly profileData = input.required<ProfileDetail[]>();
   readonly isLoading = input.required<boolean>();
-
   private profileService = inject(ProfilesService);
 
   userActionEvent(userActionType: ToolbarAction, profileId: string, event?: Event): void {

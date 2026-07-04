08fa9ec6733fc781d628950fd00e13ca4e4039b3
Jeevan Mahesha <csejeevanm@gmail.com>
2026-07-04 17:36:19 +0530

    refactor: optimize profile list loading with viewport deferral and enable router component input binding

--- Angular/ProfileManager/src/app/app.config.ts
+++ Angular/ProfileManager/src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { provideFirebase } from './firebase/provide-firebase';
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideFirebase(environment.firebase),
   ],
 };
